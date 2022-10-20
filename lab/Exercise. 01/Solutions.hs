6635a7d96148afb377cb2c3fd8f83dcbcf877b70
Martin Skachkov <mgsemailbox@gmail.com>
2022-10-20 10:20:50 +0300

    add solutions

--- lab/Exercise. 01/Solutions.hs
+++ lab/Exercise. 01/Solutions.hs
@@ -66,6 +66,7 @@ beats _ _ = False
 -- showAnimal
 data Animal = Dog Breed
             | Cat Color
+            | Add AnimalName
   deriving Show
 
 data Color = Orange | Black | White
@@ -95,22 +96,53 @@ integerToNat :: Integer -> Nat
 integerToNat 0 = Zero
 integerToNat n = (Succ (integerToNat (n - 1)))
 
-{-
 -- evaluate manually?
+-- n = 3
+-- (Succ (integerToNat (2)))
+--        (Succ (integerToNat(1)))
+--               (Succ (integerToNat(0)))
+--                      (Zero) 
 
 -- implement
 natToInteger :: Nat -> Integer
-natToInteger = undefined
+natToInteger Zero = 0
+natToInteger (Succ n) = 1 + (natToInteger n) 
+
+-- evaluate manually?
+-- (Succ (Succ (Succ Zero)))
+-- 1 + (natToInteger (Succ (Succ Zero)))
+-- 1 + 1 + (natToInteger (Succ Zero))
+-- 1 + 1 + 1 + (natToInteger Zero)
+-- 1 + 1 + 1 + 0
+-- 3
 
 -- implement
 addNat :: Nat -> Nat -> Nat
-addNat = undefined
+addNat Zero n = n
+addNat (Succ m) n = (Succ (addNat m n))
+
+-- evaluate manually?
+-- Zero (Succ Zero) -> (Succ Zero)
+-- (Succ (Succ Zero)) (Succ Zero)
+-- (Succ (addNat (Succ Zero) (Succ Zero)))
+--       (Succ (addNat Zero) (Succ Zero))
+--              (Succ Zero)
+--(Succ (Succ (Succ Zero))) 
+
+-- test
+test :: Nat -> Nat
+test (Succ x) = x
+test x = x
+-- depending which one is first the result is different as (Succ sth.) is pattern matching and it removes the first Succ and returns sth.
+-- while test x returns the same x (non-pattern matching)
 
 -- TASK
 -- define what the "next" throw you can do is in the "usual" ordering of RPS
 -- i.e. @next x@ should be the throw that beats x
 next :: RPS -> RPS
-next = undefined
+next Scissors = Rock
+next Rock = Paper
+next Paper = Scissors
 
 -- TASK
 -- define what it means for two RPS values to be equal
@@ -123,8 +155,12 @@ next = undefined
 -- True
 -- >>> eqRPS Rock Paper
 -- False
+
 eqRPS :: RPS -> RPS -> Bool
-eqRPS = undefined
+eqRPS Rock Rock = True
+eqRPS Scissors Scissors = True
+eqRPS Paper Paper = True
+eqRPS _ _ = False
 
 -- TASK
 -- define a shorter version of beats by uisng next and eqRPS
@@ -136,38 +172,57 @@ eqRPS = undefined
 -- >>> beats' Paper Scissors
 -- True
 beats' :: RPS -> RPS -> Bool
-beats' = undefined
+beats' x y = eqRPS x (next y)
+
 
 -- TASK
 -- Your task is to model a few of the pieces of the game of Belote
 -- * implement a data type for Ranks (7 8 9 10 J etc)
-data Rank
+data Rank = Ace | King | Queen | Jack | Num Int
   deriving Show
 -- * implement a data type for Suits
-data Suit
+data Suit = Clubs | Diamonds | Hearths | Spades
   deriving Show
 -- * implement a data type for a Card
-data Card
+data Card = MkCard Rank Suit
   deriving Show
 -- * implement a data type for Contracts (all trump, no trump etc)
-data Contract
+data Contract = AllTrump | NoTrump
   deriving Show
 
 -- Given a Card and a Contract, implement a check whether the card is of a trump suit
+-- done only for all trump
+-- J; 9; A; 10; K; Q; 8; 7.
 isTrump :: Contract -> Card -> Bool
-isTrump = undefined
+isTrump AllTrump (MkCard Jack Diamonds) = True
+isTrump AllTrump (MkCard (Num 9) Diamonds) = True
+isTrump AllTrump (MkCard Ace Diamonds) = True
+isTrump AllTrump (MkCard (Num 10) Diamonds) = True
+isTrump AllTrump (MkCard King Diamonds) = True
+isTrump AllTrump (MkCard Queen Diamonds) = True
+isTrump AllTrump (MkCard (Num 8) Diamonds) = True
+isTrump AllTrump (MkCard (Num 7) Diamonds) = True
+isTrump _ _ = False
 
 -- Given a Card and a Contract, implement what the "power" of that card is as an Integer
 -- When played, a card with higher power will "beat" one with lower power
 -- You can use whatever numbers you like, as long as they reflect the rules of the game.
+-- done only for all trump
 cardPower :: Contract -> Card -> Integer
-cardPower = undefined
+cardPower AllTrump (MkCard King Diamonds) = 4
+cardPower AllTrump (MkCard Ace Diamonds) = 11
+cardPower AllTrump (MkCard Queen Diamonds) = 3
+cardPower AllTrump (MkCard Jack Diamonds) = 20
+cardPower AllTrump (MkCard (Num 10) Diamonds) = 10
+cardPower AllTrump (MkCard (Num 9) Diamonds) = 14
+cardPower _ _ = 0
 
 -- Given two Cards and a Contract, return the Card that would "win" (according to their power)
 -- when playing under the given Contract
 -- Assume that the first matched card is played first.
 fight :: Contract -> Card -> Card -> Card
-fight = undefined
+fight AllTrump (MkCard (Num 9) Diamonds) (MkCard Jack Diamonds) = (MkCard Jack Diamonds)
+-- ...
 
 -- TASK
 -- multiply two @Nat@s recursively, much like we did with Ints last time
@@ -177,7 +232,9 @@ fight = undefined
 -- >>> multNat (integerToNat 2) (integerToNat 3)
 -- Suc (Suc (Suc (Suc (Suc (Suc Zero)))))
 multNat :: Nat -> Nat -> Nat
-multNat = undefined
+multNat Zero m = Zero
+multNat n Zero = Zero
+multNat (Succ n) m = addNat (multNat n m) m
 
 -- TASK
 -- calculate the larger of two @Nat@s recursively
@@ -189,7 +246,9 @@ multNat = undefined
 -- >>> maxNat (Suc (Suc Zero)) (Suc (Suc (Suc (Suc Zero))))
 -- Suc (Suc (Suc (Suc Zero)))
 maxNat :: Nat -> Nat -> Nat
-maxNat = undefined
+maxNat x Zero = x
+maxNat Zero y = y
+maxNat (Succ x) (Succ y) = Succ(maxNat x y) -- we decrement both sides with one each time and the first that reaches Zero is the smaller one so we return the bigger one
 
 -- TASK
 -- Ordering is a datatype that is made to mean "the result of a comparison" or "the ordering between two things"
@@ -204,8 +263,12 @@ maxNat = undefined
 -- LT
 -- >>> compareNat (Suc Zero) Zero
 -- GT
+
 compareNat :: Nat -> Nat -> Ordering
-compareNat = undefined
+compareNat Zero Zero = EQ
+compareNat Zero y = LT
+compareNat x Zero = GT
+compareNat (Succ x) (Succ y) = compareNat x y
 
 -- README
 -- the "syntax" for a very basic "calculator" datatype
@@ -219,6 +282,7 @@ data Expr
   = Val Integer
   | Plus Expr Expr
   | Mult Expr Expr
+  | If Expr
   deriving Show
 
 -- README - SECTIONS
@@ -264,7 +328,10 @@ infixr 8 `Mult`
 -- >>> eval (Val 3 `Plus` Val 3 `Mult` Val 7)
 -- 24
 eval :: Expr -> Integer
-eval = undefined -}
+eval (Val x) = x
+eval (Plus x y) = eval x + eval y
+eval (Mult x y) = eval x * eval y
+--eval (If v) = (eval v)
 
 -- TASK
 -- add an If expression to our Expr language
@@ -274,8 +341,13 @@ eval = undefined -}
 
 -- TASK
 -- add a name to the Animal type
---
+data AnimalName = MkName String
+  deriving Show
+
+animalName = MkName "Marto"
 -- is there more than one way to add it to Animal?
 -- which way would be more convenient for the implementation of the following function?
--- introduce :: Animal -> String
+introduce :: Animal -> String
+introduce (Add (MkName x)) = x
+introduce _ = ""
 -- which shows the name of the animal
