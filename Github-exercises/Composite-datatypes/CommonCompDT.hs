6759d069d89678de70b9f6167909837639f1ab76
Martin Skachkov <mgsemailbox@gmail.com>
2022-11-02 11:30:54 +0200

    update

--- Github-exercises/Composite-datatypes/CommonCompDT.hs
+++ Github-exercises/Composite-datatypes/CommonCompDT.hs
@@ -51,3 +51,68 @@ pureMaybe val = Just val
 foldMaybe :: b -> (a -> b) -> Maybe a -> b
 foldMaybe val func Nothing = val
 foldMaybe val func (Just a) = func a
+
+mapMaybe :: (a -> b) -> Maybe a -> Maybe b
+mapMaybe func (Just a) = Just (func a)
+mapMaybe func Nothing = Nothing
+
+applyMaybe :: Maybe (a -> b) -> Maybe a -> Maybe b
+applyMaybe (Just func) (Just a) = Just (func a)
+applyMaybe Nothing (Just a) = Nothing
+applyMaybe (Just func) Nothing = Nothing
+
+bindMaybe :: (a -> Maybe b) -> Maybe a -> Maybe b
+bindMaybe func (Just a) = func a
+bindMaybe func Nothing = Nothing
+
+-- Tuples
+-- A tuple is an ad-hoc collection of values that can be of different types
+
+tuple :: (Int, String)
+tuple = (42, "Forty-Two")
+
+tuple' :: (Int, String, Bool)
+tuple' = (42, "Forty-Two", False)
+
+tuple'' :: (Int, String, Bool, Float)
+tuple'' = (42, "Forty-Two", False, 1337.0)
+
+-- In many ways a tuple is the same as a record, except we are not naming the different components or the constructor.
+
+-- Lists
+-- data [] a
+--  = []
+--  | a : [a]
+
+data List a
+  = EmptyList -- This is commonly called `Nil`
+  | -- | `a` here is commonly called the "head" of the list, and the list it is connected to is
+    -- commonly called the "tail".
+    Prepend a (List a) -- This is commonly called `Cons`
+
+maybeFirstElement :: [a] -> Maybe a
+maybeFirstElement (a : _) = Just a
+maybeFirstElement [] = Nothing
+
+maybeFirstElement' :: [a] -> Maybe a
+maybeFirstElement' list = case list of
+  a : _ -> Just a
+  [] -> Nothing
+
+maybeFirstTwoElements :: [a] -> Maybe (a, a)
+maybeFirstTwoElements (a : b : _) = Just (a, b)
+maybeFirstTwoElements [] = Nothing
+
+maybeFirstTwoElements' :: [a] -> Maybe (a, a)
+maybeFirstTwoElements' list = case list of
+  a : b : _ -> Just (a, b)
+  [] -> Nothing
+
+maybeFirstAndRest :: [a] -> Maybe (a, [a])
+maybeFirstAndRest (a : rest) = Just (a, rest)
+maybeFirstAndRest _anyOtherCase = Nothing
+
+-- We can also match to an exact structure of a list
+maybeExactlyTwoElements :: [a] -> Maybe (a, a)
+maybeExactlyTwoElements [a, b] = Just (a, b)
+maybeExactlyTwoElements _anyOtherCase = Nothing
